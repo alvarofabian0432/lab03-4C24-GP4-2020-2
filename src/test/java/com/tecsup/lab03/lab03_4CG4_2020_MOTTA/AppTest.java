1f378b189f7a50408fde58b79b3a4f93bcd0cf4d
Alvaro Fabian <alvarofabian0432@gmail.com>
2020-10-06 16:25:20 -0500

    Primer Commit

---
+++ src/test/java/com/tecsup/lab03/lab03_4CG4_2020_MOTTA/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab03.lab03_4CG4_2020_MOTTA;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
